f5ed736b4a1161168c8d5594ec32c4c94b8e2413
Otstoy <otstoy.sxe@gmail.com>
2025-07-28 18:06:01 +0300

    Set unique parameter to True for Guild model's name.

--- db/models.py
+++ db/models.py
@@ -17,7 +17,7 @@ class Skill(models.Model):
 
 
 class Guild(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
 
