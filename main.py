082078d7e484a4395f0765115a796e4ce3223300
Otstoy <otstoy.sxe@gmail.com>
2025-07-28 17:47:37 +0300

    Implemented various models in order to add records to DB from json file.

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
+import json
+
 import init_django_orm  # noqa: F401
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r+") as file:
+        data = json.load(file)
 
+    for player_name, player_info in data.items():
+        race_description = player_info["race"]["description"] \
+            if player_info["race"]["description"] else ""
+        skills = player_info.get("race", {}).get("skills", {})
+
+        race_obj, created = Race.objects.get_or_create(
+            name=player_info.get("race", {}).get("name"),
+            description=race_description
+        )
+
+        for skill in skills:
+            Skill.objects.get_or_create(
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
+                race=race_obj
+            )
+
+        guild = player_info.get("guild")
+        guild_obj, created = (Guild.objects.get_or_create(
+            name=guild.get("name"),
+            description=guild.get("description")
+            )
+            if guild else (None, False)
+        )
+
+        Player.objects.get_or_create(
+            nickname=player_name,
+            email=player_info.get("email"),
+            bio=player_info.get("bio"),
+            race=race_obj,
+            guild=guild_obj
+        )
 
 if __name__ == "__main__":
     main()
